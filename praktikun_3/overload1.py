1eeb68334b3eacdaf8ac4bfd7055b9e5d993b7c1
Rizal Trilaksana <128065896+rizaltrilaksana@users.noreply.github.com>
2023-04-09 13:06:39 +0700

    Delete praktikun_3 directory

--- praktikun_3/overload1.py
+++
@@ -1,3 +0,0 @@
-print(min(100, 2, 5, 100, 3000, 50)) 
-print(min([1, 2, 3, 4, 0])) 
-print(min("Zebra")) 
--- praktikun_3/overload2.py
+++
@@ -1,7 +0,0 @@
-a = [100, 5, 3]
-a.sort()
-print(a) 
-
-b = ["z", "d", "h"]
-b.sort()
-print(b) 
